f19781990e6a7dc546a3d7a3b41166e938b0ce76
Won Tae Jung <wontae.andyjung@gmail.com>
2021-01-17 05:27:31 -0500

    Added scrollable list to mywork section and reorganized image directory structure.

--- src/screens/MyWorkScreen.js
+++ src/screens/MyWorkScreen.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import MyWorkScrollContent from '../components/MyWorkScrollContent';
 
 function MyWorkScreen (props) {
     return <section className="mywork">
-        <div className="section-border"></div>
+        <div className="section-border">
+        </div>
+        <div class="split right">
+            <div class="centered">
+                <MyWorkScrollContent></MyWorkScrollContent>
+            </div>
+        </div>
     </section>
 }
 
